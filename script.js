1dc29cd2f33758b56a2e7ff64d64e5b1054640a6
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-11-06 20:39:30 -0500

    access the element coordinates. use the width and height from the coordinates to style the highlight width and height

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const highlight = document.createElement('span');
 highlight.classList.add('highlight');
 document.body.append(highlight);
 
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
